904fbef9467850dd63af349d734f7515b2c5e7aa
Ben Dehghan <bendehghan.biz@gmail.com>
2011-06-12 14:30:57 -0700

    initial commit to test the new git account

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestApp::Application.config.secret_token = '56f8a8cefb0186e32640988949a437c542592e6f08ea8d9ed136f63c53d1bf39684bd40b28de4bbb43de014fc4ab3a3011d424c2eac5c45a0bd012d7404822ea'
